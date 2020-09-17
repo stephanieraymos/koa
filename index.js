70902b5ed34d2f9b776e817ddc0e8d4ee1dde7fc
Stephanie Raymos <stephanieraymos@gmail.com>
2020-09-17 15:42:19 -0700

    Printing date

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Koa = require('koa');
 const app = new Koa();
 
+// Add date method to context
+app.context.date = Date();
+
+//response
 app.use(ctx => {
-  ctx.body = 'Hello';
-})
+  //print out date
+  ctx.body = `Hello on ${ctx.date}`;
+});
 
 app.listen(3000);
