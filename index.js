c5eab2c0bee387a96005cb9d50ecdcde5199348a
Stephanie Raymos <stephanieraymos@gmail.com>
2020-09-17 15:52:09 -0700

    Request with context

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const app = new Koa();
 app.context.date = Date();
 
 //response
-app.use(ctx => {
+app.use(ctx => { //ctx has req and res built in
   //use state
   ctx.state.user = 'Stephanie Raymos';
+  //request object usage
+  let from = ctx.request.origin //Where this request is originating from (localhost:3000)
+  let method = ctx.request.method //This is a get request
   //print out date
   ctx.body = `Hello ${ctx.state.user} on ${ctx.date}`;
+  console.log(from);
+  console.log(method);
 });
 
 app.listen(3000);
